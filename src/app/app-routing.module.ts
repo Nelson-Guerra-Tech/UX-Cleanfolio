c73bc453b566568bc530c22704e3ae940108108f
Nelson Guerra <kal-el@Nelsons-MBP.attlocal.net>
2023-07-17 20:58:19 -0400

    set up css variables and routeter-outlet for angular routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app.component';
+import { PortfolioSectionComponent } from './components/portfolio-section/portfolio-section.component';
 
 const routes: Routes = [
-  // {path: '', component: ''}
+  {path: '', component: HomeComponent},
+  {path: 'portfolio', component: PortfolioSectionComponent},
+  {path: '**', component: HomeComponent},
 ];
 
 @NgModule({
