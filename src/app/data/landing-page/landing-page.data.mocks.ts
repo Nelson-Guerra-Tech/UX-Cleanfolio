c65a0753c766140b62973fb4b8cb3efa2e063190
Nelson Guerra <kal-el@Nelsons-MBP.attlocal.net>
2023-07-21 18:52:20 -0400

    add minor styling to landing page and navbar links

--- src/app/data/landing-page/landing-page.data.mocks.ts
+++ src/app/data/landing-page/landing-page.data.mocks.ts
@@ -2,5 +2,5 @@ import { LandingPage } from "src/app/models/landing-page/landing-page.interface"
 
 export const landingPageDataMocks: LandingPage = {
   nameTitle: "HELLO, I'M NELSON GUERRA,",
-  description: "as a <span class='highlight'>Designer</span> and <span>Developer</span>, <br /> I know technology is better <br />when it brings people together."
+  description: 'as a <span>Designer</span> and <span>Developer</span>, <br /> I know technology is better <br />when it brings people together.'
 }
